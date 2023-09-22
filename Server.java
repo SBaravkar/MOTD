2c8eac9780fa7e4ee3fb8dce9e174d02a1869fe3
Pratiksha Gaikwad <gaikwad.pratiksha0307@gmail.com>
2023-09-22 15:54:22 -0400

    Adding comments

--- Server.java
+++ Server.java
@@ -16,7 +16,7 @@ public class Server {
     public static int messageIndex = 0;
     public static boolean isLoggedIn = false;
 
-    public static void main(String args[])
+    public static void main(String[] args)
     {
         ServerSocket myServerice = null;
         String line;
@@ -72,33 +72,37 @@ public class Server {
                         } if(loginInfo.length != 3) {
                             os.println("400 Bad Request. Invalid arguments.");
                         }
-                    } else if (line.startsWith("MSGGET")) {
+                    } else if (line.startsWith("MSGGET")) {     // The MSGGET instruction is responsible for fetching and forwarding the daily messages of the client.
+
                         if (messageIndex < messagesOfTheDay.size()) {
-                           // os.println("200 OK");
+                           // Message at the current index is retrieved.
                             String message = messagesOfTheDay.get(messageIndex);
+                            // Send the client a success response (200 OK) and the message retrieved from messagesOfTheDay.
                             os.println("200 OK " +message);
-                            messageIndex = (messageIndex + 1) % messagesOfTheDay.size();
+                            messageIndex = (messageIndex + 1) % messagesOfTheDay.size(); // Increment the Index count to print the messages in sequential manner.
                         }
-                    } else if (line.startsWith("MSGSTORE")) {
+                    } else if (line.startsWith("MSGSTORE")) { // The MSGSTORE command, which enables logged-in users to store a new message.
                         if (isLoggedIn)  {
-                            // Read and store the new message
                             os.println("200 OK");
+                            // Read the new message sent by the user.
                             String newMessage = is.readLine();
+                            // Store the new message sent by the user in the text file.
                             storeMessage(newMessage);
+                            // Sent message to client after successfully storing the new message
                             os.println("200 OK");
                         } else {
                             os.println("401 You are not currently logged in, login first.");
                         }
                     } else if (line.startsWith("LOGOUT")) {
                         if (isLoggedIn) {
-                            //os.println("200 OK");
+                            // Logout the user from the environment and allow other user to login by setting the flag to false.
                             isLoggedIn = false;
                             os.println("200 OK. Successfully Logout.");
                         }else{
                             os.println("401 You are not currently logged in, login first.");
                         }
                     } else if (line.startsWith("SHUTDOWN")) {
-                        if (isLoggedIn == true && username.equals("root")){
+                        if (isLoggedIn && username.equals("root")){  // check if user is logged in and username is root user.
                             isLoggedIn = false;
                             os.println("200 OK. Server Shutdown!");
                             System.exit(1);
@@ -130,9 +134,9 @@ public class Server {
 
     // Function to read messages from a text file
     private static ArrayList<String> readMessagesFromFile(String fileName) {
-        ArrayList<String> messages = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
-            String line;
+        ArrayList<String> messages = new ArrayList<>(); // ArrayList to store messages.
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) { /// To read the mentioned file, open it as a FileReader and wrap it in a BufferedReader.
+            String line; //store each line read from the file.
             while ((line = br.readLine()) != null) {
                 messages.add(line);
             }
@@ -141,11 +145,14 @@ public class Server {
         }
         return messages;
     }
+    // Function to store a new message in a text file
     private static void storeMessage(String message) {
         try (FileWriter fw = new FileWriter("messages.txt", true);
+             // Open the "messages.txt" file for writing using a FileWriter and the 'true' flag specifies that we want to append to the file if it exists.
              BufferedWriter bw = new BufferedWriter(fw);
              PrintWriter out = new PrintWriter(bw)) {
-             out.println(message);
+            out.println(message); // Write the 'message' string to the file.
+
         } catch (IOException e) {
             System.err.println("Error storing message: " + e.getMessage());
         }
